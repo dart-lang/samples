8b060080aa3f2588bebb02bb175014be32995b4e
John Ryan <ryjohn@google.com>
2019-09-05 13:05:24 -0700

    add copyright headers to .dart, .c,  and .h files

--- ffi/primitives/c/primitives.h
+++ ffi/primitives/c/primitives.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 int sum(int a, int b);
 int subtract(int *a, int b);
 int *multiply(int a, int b);
