8c70e977f833d96e79b7d5f68c27a7f7eda06b06
Michael Thomsen <mit@google.com>
2019-09-09 19:15:20 +0200

    Review feedback

--- ffi/system-command/windows.dart
+++ ffi/system-command/windows.dart
@@ -33,13 +33,19 @@ typedef ShellExecuteDart = int Function(
     int showCmd);
 
 int ShellExecute(String operation, String file) {
+  // Load shell32.
   final dylib = ffi.DynamicLibrary.open('shell32.dll');
+
+  // Look up the `ShellExecuteW` function.
   final ShellExecuteP =
       dylib.lookupFunction<ShellExecuteC, ShellExecuteDart>('ShellExecuteW');
 
+  // Allocate pointers to Utf8 arrays containing the command arguments.
   final operationP = Utf16.toUtf16(operation);
   final fileP = Utf16.toUtf16(file);
   const int SW_SHOWNORMAL = 1;
+
+  // Invoke the command, and free the pointers.
   var result = ShellExecuteP(
       ffi.nullptr, operationP, fileP, ffi.nullptr, ffi.nullptr, SW_SHOWNORMAL);
   operationP.free();
