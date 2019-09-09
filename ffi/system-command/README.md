1168a17cd85920ad05a5bd4f9a083cd7b041fd64
Abdulrahman Falyoun <abd.fl.19999@gmail.com>
2019-09-09 18:38:18 +0200

    Add system command example

---
+++ ffi/system-command/README.md
@@ -0,0 +1,13 @@
+FFI sample demonstrating invoking operating system commands:
+
+  * `macos.dart`: Invoke the `system()` command in `libSystem.dylib`
+  * `linux.dart`: Invoke the `system()` command in `libc.so.6`
+  * `windows.dart`: Invoke the `ShellExecute` command in `shell32.dll`
+
+To run these samples:
+  * Run `pub get` to get the dependencies
+  * Run `dart macos.dart`, `dart linux.dart`, or `dart windows.dart`
+    depending on the OS you are on.
+
+When run, you should see the system-default browser launch and load the
+Dart website, https://dart.dev
---
+++ ffi/system-command/macos.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart';
+
+main() {
+  var result = system('open http://dart.dev');
+  print('Result: $result');
+}
+
+/*
+#include <stdlib.h>
+int system(const char *string);
+
+https://man.openbsd.org/system.3
+*/
+
+// C header typedef:
+typedef SystemC = ffi.Int32 Function(ffi.Pointer<Utf8> command);
+
+// Dart header typedef
+typedef SystemDart = int Function(ffi.Pointer<Utf8> command);
+
+int system(String command) {
+  // Load `stdlib`. On MacOS this is in libSystem.dylib.
+  final dylib = ffi.DynamicLibrary.open('libSystem.dylib');
+  // Look up the `system` function.
+  final systemP = dylib.lookupFunction<SystemC, SystemDart>('system');
+
+  // Allocate a pointer to a Utf8 array containing our command.
+  final cmdP = Utf8.toUtf8(command);
+
+  // Invoke the command, and return the result.
+  return systemP(cmdP);
+}
