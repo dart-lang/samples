3a39d63182d9aee2869fdd3676674205b79a3691
Abdulrahman Falyoun <abd.fl.19999@gmail.com>
2019-09-25 02:47:30 +0530

    refrence for windows users

--- ffi/hello_world/c/hello.c
+++ ffi/hello_world/c/hello.c
@@ -11,6 +11,10 @@ int main()
     return 0;
 }
 
+// Note:
+// ---only on Windows---
+// Every function needs to be exported to be able to access the functions by dart.
+// Refer: https://stackoverflow.com/q/225432/8608146
 void hello_world()
 {
     printf("Hello World\n");
