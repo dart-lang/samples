ebf797e0e5290d2d347ec441fc4de51a4b487fb6
John Ryan <ryjohn@google.com>
2019-10-17 13:10:44 -0700

    add native app

---
+++ native_app/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:native_app/native_app.dart' as native_app;
+
+main(List<String> arguments) {
+  print('Hello world: ${native_app.calculate()}!');
+}
---
+++ native_app/test/native_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:native_app/native_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
