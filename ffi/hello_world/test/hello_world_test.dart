c949643aea2920b299489355075e6fd1c3b37f43
Abdulrahman Falyoun <abd.fl.19999@gmail.com>
2019-12-18 04:59:15 +1100

    Using CMake for cross platform Makefile generation (#23)
    * Migrate to CMake
    
    * Make linux happy
    
    * Make CMake work inside google/dart Docker image
    
    * Tidyup
    
    * Try building the hello_library
    
    * Fix up chdir target and add cmake

--- ffi/hello_world/test/hello_world_test.dart
+++ ffi/hello_world/test/hello_world_test.dart
@@ -6,28 +6,14 @@ import 'package:test/test.dart';
 // README.
 void main() async {
   group('hello_world', () {
-    test('make dylib + execute', () async {
-      // run 'make clean'
-      var clean = await Process.run('make', ['-f', 'LinuxMakefile', 'clean'],
-          workingDirectory: 'c');
-      expect(clean.exitCode, 0);
-
-      // run 'make so'
-      var dynamicLib = await Process.run('make', ['-f', 'LinuxMakefile', 'so'],
-          workingDirectory: 'c');
-      expect(dynamicLib.exitCode, 0);
-
-      // Verify dynamic library was created
-      var file = File('./hello_world.so');
-      expect(await file.exists(), true);
-
+    test('execution', () async {
       // Run the Dart script
       var dartProcess = await Process.run('dart', ['hello.dart']);
-      expect(dartProcess.exitCode, equals(0));
 
       // Verify program output
       expect(dartProcess.stderr, isEmpty);
       expect(dartProcess.stdout, equals('Hello World\n'));
+      expect(dartProcess.exitCode, equals(0));
     });
   });
 }
