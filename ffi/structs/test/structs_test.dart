06356e1f6682a6433e909258090e62837fee150b
dhartman1 <dhartman1.dh@gmail.com>
2020-02-19 10:00:46 -0500

    Fix native library name in structs_test.dart
    In ead90d96bf348a19dcf1015218133c36a96b85b6 I forgot to update the test file to also include the "lib" prefix for the native library name.

--- ffi/structs/test/structs_test.dart
+++ ffi/structs/test/structs_test.dart
@@ -18,7 +18,7 @@ void main() async {
       expect(make.exitCode, 0);
 
       // Verify dynamic library was created
-      var file = File('./structs_library/structs.so');
+      var file = File('./structs_library/libstructs.so');
       expect(await file.exists(), true);
 
       // Run the Dart script
