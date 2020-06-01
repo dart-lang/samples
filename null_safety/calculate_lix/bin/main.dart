a544a1f747a1404ea42ac075db477cf1e214bc01
Michael Thomsen <mit@google.com>
2020-06-01 14:22:28 +0200

    Add an a sample CLI app that uses null safety (#55)

---
+++ null_safety/calculate_lix/bin/main.dart
@@ -0,0 +1,39 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:calculate_lix/lix.dart';
+
+void main(List<String> arguments) {
+  // Parse the arguments; we expect a single argument containing the file name.
+  if (arguments.length != 1) {
+    printUsage();
+    exit(64);
+  } else {
+    // Get the file name.
+    //
+    // TIP: We're using null safe versions of the core libraries, and the type
+    // of `arguments` is `List` which means that arguments cannot contain null
+    // elements. As a result, the Dart analyzer knows that `fileName` isn't
+    // null.
+    final fileName = arguments[0];
+    print("Calculating Lix of '$fileName'");
+
+    // Calculate lix.
+    try {
+      final l = Lix.fromString(File(fileName).readAsStringSync());
+      print("Lix is: ${l.readability}, ${l.describe()} to read ("
+          "words: ${l.words}, long words: ${l.longWords}, "
+          "periods: ${l.periods}).");
+    } catch (ArgumentError) {
+      print(
+          'Invalid input, could not calculate lix!\nThe input text must contain at least one full sentence.');
+    }
+  }
+}
+
+void printUsage() {
+  print('Usage: calculate_lix <text file>');
+}
---
+++ null_safety/calculate_lix/lib/lix.dart
@@ -0,0 +1,86 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class Lix {
+  // Based on:
+  // https://readabilityformulas.com/the-LIX-readability-formula.php.
+  int words; // Number of words in general.
+
+  int longWords; // Number of words with more than 6 characters.
+
+  int periods; // Number of periods.
+
+  // TIP: `readability` isn't passed to the constructor, but calculated. By
+  // adding the late keyword we tell the analyzer that it will be initialized
+  // later in the program.
+  late int readability; // The calculated lix readability index .
+
+  // TIP: Because the fields are all non-nullable, the constructor must
+  // initialize them all. We can either set a default value, or like here make
+  // the fields 'required', meaning that a value must be passed to the
+  // constructor. 'required' is a new keyword introduced as part of null safety
+  // which replaces the previous '@required' annotatation.
+  Lix({
+    required this.words,
+    required this.longWords,
+    required this.periods,
+  }) {
+    readability = this._calculate();
+  }
+
+  factory Lix.fromString(String text) {
+    // Count periods: . : ; ! ?
+    var periods = (RegExp(r"[.:;!?]").allMatches(text).length);
+
+    // Count words.
+    //
+    // TIP: We're using null safe versions of the core libraries, so it's clear
+    // from the signature of split() that it returns a non-null result.
+    var allWords = text.split(RegExp(r"\W+"));
+    var words = allWords.length;
+    var longWords = allWords.where((w) => w.length > 6).toList().length;
+
+    return Lix(words: words, longWords: longWords, periods: periods);
+  }
+
+  // TIP: Notice how we declare a non-nullable uninitalized `result` variable,
+  // yet we can return is as a non-nullable result without getting an error.
+  //
+  // This is possible due to "definite assignement": The Dart analyzer
+  // determines that a value has definitely been assigned before we return. Try
+  // removing the assignment to `result` in our of the if/else code branches,
+  // and notice how an error then appears in the return statement.
+  int _calculate() {
+    int result;
+
+    if (words == 0 || periods == 0) {
+      throw (ArgumentError('Text must contain at least one full sentence.'));
+    } else {
+      final sentenceLength = words / periods;
+      final wordLength = (longWords * 100) / words;
+      result = (sentenceLength + wordLength).round();
+    }
+
+    return result;
+  }
+
+  // TIP: Notice how flow analysis knows that every branch returns a value for
+  // `readability` and so it's ok to have a non-nullable return type (try
+  // removing the last else-statement).
+  String describe() {
+    if (readability > 0 && readability < 20) {
+      return 'very easy';
+    } else if (readability < 30) {
+      return 'easy';
+    } else if (readability < 40) {
+      return 'a little hard';
+    } else if (readability < 50) {
+      return 'hard';
+    } else if (readability < 60) {
+      return 'very hard';
+    } else {
+      return 'unknown';
+    }
+  }
+}
