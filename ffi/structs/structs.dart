874317f0eaa614b7c6d2d8366d01919d70358d3a
Abdulrahman Falyoun <abd.fl.19999@gmail.com>
2020-10-07 10:20:48 +0300

    Adopting to the new style of typedef in dart by removing the name of arguments as they're not needed and resturcturing

--- ffi/structs/structs.dart
+++ ffi/structs/structs.dart
@@ -38,20 +38,58 @@ class Place extends Struct {
 typedef hello_world_func = Pointer<Utf8> Function();
 
 // C string parameter pointer function - char *reverse(char *str, int length);
-typedef reverse_func = Pointer<Utf8> Function(Pointer<Utf8> str, Int32 length);
-typedef Reverse = Pointer<Utf8> Function(Pointer<Utf8> str, int length);
+typedef reverse_func = Pointer<Utf8> Function(Pointer<Utf8>, Int32);
+typedef Reverse = Pointer<Utf8> Function(Pointer<Utf8>, int);
 
 // C struct pointer return function - struct Coordinate *create_coordinate(double latitude, double longitude);
-typedef create_coordinate_func = Pointer<Coordinate> Function(
-    Double latitude, Double longitude);
-typedef CreateCoordinate = Pointer<Coordinate> Function(
-    double latitude, double longitude);
+typedef create_coordinate_func = Pointer<Coordinate> Function(Double, Double);
+typedef CreateCoordinate = Pointer<Coordinate> Function(double, double);
 
 // C struct pointer return function - struct Place *create_place(char *name, double latitude, double longitude);
 typedef create_place_func = Pointer<Place> Function(
-    Pointer<Utf8> name, Double latitude, Double longitude);
-typedef CreatePlace = Pointer<Place> Function(
-    Pointer<Utf8> name, double latitude, double longitude);
+    Pointer<Utf8>, Double, Double);
+typedef CreatePlace = Pointer<Place> Function(Pointer<Utf8>, double, double);
+
+void testHelloWorldFunction(final DynamicLibrary dynamicLibrary) {
+  final helloWorldPointer =
+      dynamicLibrary.lookup<NativeFunction<hello_world_func>>('hello_world');
+  final helloWorld = helloWorldPointer.asFunction<hello_world_func>();
+  final messagePointer = helloWorld();
+  final message = Utf8.fromUtf8(messagePointer);
+  print('$message');
+}
+
+void testReverseFunction(final DynamicLibrary dynamicLibrary) {
+  final reversePointer =
+      dynamicLibrary.lookup<NativeFunction<reverse_func>>('reverse');
+  final reverse = reversePointer.asFunction<Reverse>();
+  final reversedMessage = Utf8.fromUtf8(reverse(Utf8.toUtf8('backwards'), 9));
+  print('$reversedMessage');
+}
+
+void testCoordinatePointerCreation(final DynamicLibrary dynamicLibrary) {
+  final createCoordinatePointer = dynamicLibrary
+      .lookup<NativeFunction<create_coordinate_func>>('create_coordinate');
+  final createCoordinate =
+      createCoordinatePointer.asFunction<CreateCoordinate>();
+  final coordinatePointer = createCoordinate(1.0, 2.0);
+  final coordinate = coordinatePointer.ref;
+  print('Coordinate: ${coordinate.latitude}, ${coordinate.longitude}');
+}
+
+void testPlacePointerCreation(final DynamicLibrary dynamicLibrary) {
+  final String messagePointer = 'Dummy Message For Testing Purposes';
+
+  final createPlacePointer =
+      dynamicLibrary.lookup<NativeFunction<create_place_func>>('create_place');
+  final createPlace = createPlacePointer.asFunction<CreatePlace>();
+  final placePointer = createPlace(Utf8.toUtf8(messagePointer), 3.5, 4.6);
+  final place = placePointer.ref;
+  final placeName = Utf8.fromUtf8(place.name);
+  final placeCoordinate = place.coordinate.ref;
+  print(
+      'Place is called $placeName at ${placeCoordinate.latitude}, ${placeCoordinate.longitude}');
+}
 
 main() {
   var path = './structs_library/libstructs.so';
@@ -59,33 +97,8 @@ main() {
   if (Platform.isWindows) path = r'structs_library\Debug\structs.dll';
   final dylib = DynamicLibrary.open(path);
 
-  final helloWorldPointer =
-      dylib.lookup<NativeFunction<hello_world_func>>('hello_world');
-  final helloWorld = helloWorldPointer.asFunction<hello_world_func>();
-  final messagePointer = helloWorld();
-  final message = Utf8.fromUtf8(messagePointer);
-  print('$message');
-
-  final reversePointer = dylib.lookup<NativeFunction<reverse_func>>('reverse');
-  final reverse = reversePointer.asFunction<Reverse>();
-  final reversedMessage = Utf8.fromUtf8(reverse(Utf8.toUtf8('backwards'), 9));
-  print('$reversedMessage');
-
-  final createCoordinatePointer =
-      dylib.lookup<NativeFunction<create_coordinate_func>>('create_coordinate');
-  final createCoordinate =
-      createCoordinatePointer.asFunction<CreateCoordinate>();
-  final coordinatePointer = createCoordinate(1.0, 2.0);
-  final coordinate = coordinatePointer.ref;
-  print('Coordinate: ${coordinate.latitude}, ${coordinate.longitude}');
-
-  final createPlacePointer =
-      dylib.lookup<NativeFunction<create_place_func>>('create_place');
-  final createPlace = createPlacePointer.asFunction<CreatePlace>();
-  final placePointer = createPlace(messagePointer, 3.5, 4.6);
-  final place = placePointer.ref;
-  final placeName = Utf8.fromUtf8(place.name);
-  final placeCoordinate = place.coordinate.ref;
-  print(
-      'Place is called $placeName at ${placeCoordinate.latitude}, ${placeCoordinate.longitude}');
+  testHelloWorldFunction(dylib);
+  testReverseFunction(dylib);
+  testCoordinatePointerCreation(dylib);
+  testPlacePointerCreation(dylib);
 }
