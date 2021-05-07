ea22dd43c5a2649b9c95f93eee478798ffef9e75
Sameer Kashyap <40424087+Sameerkash@users.noreply.github.com>
2021-05-08 00:17:07 +0530

    Add shelf static to server/simple (#103)

--- server/simple/bin/server.dart
+++ server/simple/bin/server.dart
@@ -7,23 +7,33 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_static/shelf_static.dart';
 
 Future main() async {
   // Find port to listen on from environment variable.
   // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
+  // Serve static files from a file system directory.
+  final _staticHandler =
+      createStaticHandler('public', defaultDocument: 'index.html');
+
+  // Cascade helps in chaining multiple handlers together.
+  final _cascade = Cascade().add(_staticHandler).add(_router);
+
   // Serve handler on given port.
   final server = await serve(
-    _router,
+    _cascade.handler,
     InternetAddress.anyIPv4, // Allows external connections
     port,
   );
+
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
+// Router instance to handler requests.
 final _router = Router()
-  ..get('/', _helloWorldHandler)
+  ..get('/helloworld', _helloWorldHandler)
   ..get('/time',
       (request) => Response.ok(DateTime.now().toUtc().toIso8601String()));
 
