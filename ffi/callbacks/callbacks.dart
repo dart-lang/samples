9d2cbf01fa603d4566a7a55a116fd754af51a8f7
Michael Schwartz <mykesx@gmail.com>
2021-05-24 08:56:08 -0700

    double check contributing guildelines

--- ffi/callbacks/callbacks.dart
+++ ffi/callbacks/callbacks.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:ffi';
 import 'dart:io' show Platform, Directory;
 import 'package:path/path.dart' as path;
@@ -7,9 +11,9 @@ void my_callback(int n) {
   print('called back $n');
 }
 
-// the main program
+// The main program
 main() {
-  // Open the dynamic library
+  // Open the dynamic library.
   var libraryPath =
       path.join(Directory.current.path, 'callbacks_library', 'libcallbacks.so');
 
@@ -25,7 +29,7 @@ main() {
 
   final dl = DynamicLibrary.open(libraryPath);
 
-  // find our PerformCallback function defined in the dynamic library
+  // Find our PerformCallback function defined in the dynamic library.
   final performCallback = dl.lookupFunction<
           Void Function(
               Pointer<NativeFunction<Void Function(IntPtr)>> functionPointer),
@@ -33,10 +37,10 @@ main() {
               Pointer<NativeFunction<Void Function(IntPtr)>> functionPointer)>(
       'PerformCallback');
 
-  // cast our Dart callback into a function pointer suitable to pass to C routine
+  // Cast our Dart callback into a function pointer suitable to pass to C routine.
   final my_callbackFP =
       Pointer.fromFunction<Void Function(IntPtr)>(my_callback);
 
-  // call the C function with our callback
+  // Call the C function with our callback.
   performCallback(my_callbackFP);
 }
