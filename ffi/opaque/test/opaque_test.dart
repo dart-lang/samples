042fa5d1ae82a9c1c9e53468ef6953e7e716a5d9
Michael Schwartz <mykesx@gmail.com>
2021-05-27 14:00:52 -0700

    replace references to hello_world to opaque, and fixed the opaque_test.dart file

--- ffi/opaque/test/opaque_test.dart
+++ ffi/opaque/test/opaque_test.dart
@@ -6,29 +6,29 @@ import 'package:test_utils/test_utils.dart';
 // These tests are Linux-only. For platform-specific instructions, see the
 // README.
 void main() async {
-  group('hello_world', () {
+ group('opaque_world', () {
     test('make dylib + execute', () async {
       // run 'cmake .'
       var cmake =
-          await Process.run('cmake', ['.'], workingDirectory: 'hello_library');
+          await Process.run('cmake', ['.'], workingDirectory: 'opaque_library');
       expect(cmake.exitCode, 0);
 
       // run 'make'
       var make =
-          await Process.run('make', [], workingDirectory: 'hello_library');
+          await Process.run('make', [], workingDirectory: 'opaque_library');
       expect(make.exitCode, 0);
 
       // Verify dynamic library was created (Linux only)
-      var filePath = getLibraryFilePath('hello_library', 'hello');
+      var filePath = getLibraryFilePath('opaque_library', 'opaque');
       var file = File(filePath);
       expect(await file.exists(), true, reason: '$filePath does not exist');
 
       // Run the Dart script
-      var dartProcess = await Process.run('dart', ['hello.dart']);
+      var dartProcess = await Process.run('dart', ['opaque.dart']);
 
       // Verify program output
       expect(dartProcess.stderr, isEmpty);
-      expect(dartProcess.stdout, equals('Hello World\n'));
+      expect(dartProcess.stdout, equals('main called\n'));
       expect(dartProcess.exitCode, equals(0));
     });
   });
