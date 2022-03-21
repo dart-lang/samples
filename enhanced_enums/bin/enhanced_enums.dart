3fc08e68ab9d7442eb822f5134993ab062f9134a
Miguel Beltran <m@beltran.work>
2022-03-21 16:05:09 +0100

    add template project

---
+++ enhanced_enums/bin/enhanced_enums.dart
@@ -0,0 +1,5 @@
+import 'package:enhanced_enums/enhanced_enums.dart' as enhanced_enums;
+
+void main(List<String> arguments) {
+  print('Hello world: ${enhanced_enums.calculate()}!');
+}
---
+++ enhanced_enums/test/enhanced_enums_test.dart
@@ -0,0 +1,8 @@
+import 'package:enhanced_enums/enhanced_enums.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
