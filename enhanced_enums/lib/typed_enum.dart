66820c143de127af7f853a603d21df6ad8878253
Miguel Beltran <m@beltran.work>
2022-03-22 20:52:41 +0100

    Update enhanced_enums/lib/typed_enum.dart
    Co-authored-by: John Ryan <ryjohn@google.com>

--- enhanced_enums/lib/typed_enum.dart
+++ enhanced_enums/lib/typed_enum.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Enum with a Type.
+/// Enum with a generic type.
 ///
 /// The type [T] is used to define the type of [List] the enum contains.
 enum EnumWithType<T> {
