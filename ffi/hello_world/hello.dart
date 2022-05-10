6fe5616c8217f87c1cdf9c5c4ceb211bc6210310
John Ryan <ryjohn@google.com>
2022-05-10 15:26:08 -0700

    Run mono_repo for beta channel

--- ffi/hello_world/hello.dart
+++ ffi/hello_world/hello.dart
@@ -16,10 +16,12 @@ void main() {
   // Open the dynamic library
   var libraryPath =
       path.join(Directory.current.path, 'hello_library', 'libhello.so');
+
   if (Platform.isMacOS) {
     libraryPath =
         path.join(Directory.current.path, 'hello_library', 'libhello.dylib');
   }
+
   if (Platform.isWindows) {
     libraryPath = path.join(
         Directory.current.path, 'hello_library', 'Debug', 'hello.dll');
