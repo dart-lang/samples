1491bb2399e9260882aba6a939e0a0151053639f
Kevin Moore <kevmoo@users.noreply.github.com>
2026-01-12 16:26:20 -0800

    [simple/server] include a script to deploy using cloud run ononly (#238)

--- server/simple/README.md
+++ server/simple/README.md
@@ -7,15 +7,34 @@ A simple Dart HTTP server using [package:shelf](https://pub.dev/packages/shelf).
   [Cloud Run](https://cloud.google.com/run).)
 - Includes `Dockerfile` for easy containerization
 
+## Run locally
+
 To run this server locally, run as follows:
 
 ```bash
 $ dart run bin/server.dart
 ```
 
-To deploy on [Cloud Run](https://cloud.google.com/run), click here
+## Deploy to Google Cloud Run
+
+### With Docker
+
+To deploy on [Cloud Run](https://cloud.google.com/run), click
 
 [![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run/?git_repo=https://github.com/dart-lang/samples.git&dir=server/simple)
 
 or follow
 [these instructions](https://cloud.google.com/run/docs/quickstarts/build-and-deploy/other).
+
+### With OS-only runtime
+
+Use [`tool/deploy_source.sh`](tool/deploy_source.sh) to build and 
+deploy using the
+[OS-only runtimes](https://docs.cloud.google.com/docs/buildpacks/osonly)
+feature.
+
+> [!NOTE]
+> As of January 2026, this feature is in "Preview" and
+> requires the ["beta" gcloud component][gcloud-beta].
+
+[cloud-beta]: https://docs.cloud.google.com/sdk/docs/components#alpha_and_beta_components
---
+++ server/simple/tool/deploy_source.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# This script deploys the server to Google Cloud Run using source deployment.
+# See https://docs.cloud.google.com/docs/buildpacks/osonly
+# This feature (as of 2026-01-08) is in "Preview" and requires the "beta" gcloud component.
+
+set -e
+
+# Get the directory of this script.
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Get the parent directory of the script's directory.
+EXPECTED_DIR=$(dirname "$SCRIPT_DIR")
+CURRENT_DIR=$(pwd)
+
+if [ "$CURRENT_DIR" != "$EXPECTED_DIR" ]; then
+  echo "Error: This script must be run from: $EXPECTED_DIR" >&2
+  exit 1
+fi
+
+# Configure project from environment variables.
+DEFAULT_SERVICE_NAME="dart-sample"
+DEFAULT_GCP_REGION="us-central1"
+
+if [ -z "${GCP_PROJECT}" ]; then
+  echo "Warning: GCP_PROJECT environment variable not set. Using default project from gcloud configuration." >&2
+  PROJECT_ARG=""
+else
+  PROJECT_ARG="--project=${GCP_PROJECT}"
+fi
+
+if [ -z "${SERVICE_NAME}" ]; then
+  echo "Warning: SERVICE_NAME environment variable not set, defaulting to '${DEFAULT_SERVICE_NAME}'." >&2
+  SERVICE_NAME="${DEFAULT_SERVICE_NAME}"
+fi
+
+if [ -z "${GCP_REGION}" ]; then
+  echo "Warning: GCP_REGION environment variable not set, defaulting to '${DEFAULT_GCP_REGION}'." >&2
+  GCP_REGION="${DEFAULT_GCP_REGION}"
+fi
+
+BUILD_DIR="build"
+BINARY_LOCATION="bin/server"
+
+# Build and prepare Dart source.
+mkdir -p ${BUILD_DIR}/bin
+dart compile exe bin/server.dart -o ${BUILD_DIR}/${BINARY_LOCATION} --target-arch x64 --target-os linux
+cp -r public ${BUILD_DIR}/
+
+# Deploy to Google Cloud Run without using build.
+gcloud beta run deploy ${SERVICE_NAME} \
+  ${PROJECT_ARG} \
+  --region=${GCP_REGION} \
+  --allow-unauthenticated \
+  --no-build \
+  --base-image=osonly24 \
+  --source ${BUILD_DIR} \
+  --command=${BINARY_LOCATION}
